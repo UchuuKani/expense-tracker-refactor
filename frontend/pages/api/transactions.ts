9b5efbedf3971c5420782b6e83729719af0d9d6e
alex <probesalesman@gmail.com>
2021-11-20 15:35:59 -0500

    wip api/transactions file (no route params)

--- frontend/pages/api/transactions.ts
+++ frontend/pages/api/transactions.ts
@@ -1,33 +1,31 @@
-import { IncomingMessage, ServerResponse } from "http";
+import { NextApiRequest, NextApiResponse } from "next";
 import {
   tempTransactionStore,
   getKeyInEntity,
   addToEntity,
 } from "./utils/tempEntities";
 
-export default (req: IncomingMessage, res: ServerResponse) => {
-  const { id: transactionId } = req?.query || {};
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const transactionId = req?.query?.id;
   const postBody = req?.body;
-  console.log("yetr?", typeof postBody, postBody);
-  switch (true) {
-    case req.method === "GET": {
+
+  if (req.method === "GET") {
+    if (typeof transactionId === "string") {
       const foundTransaction = getKeyInEntity(
         tempTransactionStore,
         transactionId
       );
 
       if (foundTransaction) return res.status(200).json(foundTransaction);
-      else return res.send(404);
+      return res.send(404);
     }
-    case req.method === "POST": {
-      // check to make sure we have a body, if not error out or something
-      const parsedBody = JSON.parse(postBody);
-      const attemptToAddTransaction = addToEntity("TRANSACTION", parsedBody);
-      console.log("attempt plz", attemptToAddTransaction);
-      return res.send(attemptToAddTransaction);
-    }
+  } else if (req.method === "POST") {
+    // check to make sure we have a body, if not error out or something
+    const parsedBody = JSON.parse(postBody);
+    const attemptToAddTransaction = addToEntity("TRANSACTION", parsedBody);
 
-    default:
-      return res.sendStatus(500);
+    return res.send(attemptToAddTransaction);
   }
+
+  return res.status(500).send("Internal server error");
 };
