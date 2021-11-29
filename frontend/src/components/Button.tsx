e116a5f42b0de8def9e35d844469f3df1cd277bb
alex <probesalesman@gmail.com>
2021-11-28 23:24:07 -0500

    style and prop changes for Button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 
+type ButtonTypes = "button" | "submit" | "reset" | undefined;
+
 interface IProps {
-  handleClick: (event: React.MouseEvent<HTMLElement>) => void;
+  handleClick?: (event: React.MouseEvent<HTMLElement>) => void;
+  type?: ButtonTypes;
+  style?: React.CSSProperties;
+  classes?: string;
+  onFocus?: (event: React.FocusEvent) => void;
+  onBlur?: (event: React.FocusEvent) => void;
 }
 
-const Button: React.FC<IProps> = ({ children, handleClick }) => (
-  <button className="button-base" onClick={handleClick}>
+const Button: React.FC<IProps> = ({
+  children,
+  handleClick,
+  type = "button",
+  style = {},
+  classes = "",
+}) => (
+  <button
+    type={type}
+    className={`button-base ${classes}`}
+    style={style}
+    onClick={handleClick}
+  >
     {children}
   </button>
 );
