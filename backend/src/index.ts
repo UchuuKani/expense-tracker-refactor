93ee1d8f37f71e181d2511a8f870dee461de1c2c
alex <probesalesman@gmail.com>
2021-11-28 20:36:28 -0500

    sets up cors for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+const cors = require("cors");
 const app = express();
 const PORT = 8080;
 
+app.use(cors());
+
 // figure out why I need to add a "_" to unused variables even when "noUnusedLocals" in tsconfig is set to false
-app.get("/", (_req, res, _next) => {
-  return res.send("Hello from express!");
+// 11/28/2021 - realize the type errors are popping up because of the "noUnusedParameters" option in tsconfig. Can append a "_" to get around this, or
+// turn off the rule - for now will use the "_" approach
+app.get("/", (_req: Request, res: Response, _next: NextFunction) => {
+  return res.json("hi");
 });
 
 app.listen(PORT, function appListener() {
