89ec2e6a48d3aa8fb87eb3274be517277a3b6cfa
alex <probesalesman@gmail.com>
2021-12-02 12:21:42 -0500

    adds util functions to check string[] for duplicates and to convert a string to lowercase and trim (normalize)

--- frontend/src/global-utils/index.ts
+++ frontend/src/global-utils/index.ts
@@ -6,3 +6,23 @@ export function trimString(input: string) {
 export function isEmptyString(input: string) {
   return trimString(input) === "";
 }
+
+export function normalizeString(input: string): string | undefined {
+  if (!input) return;
+
+  return input.toLowerCase().trim();
+}
+
+export function containsDuplicates(
+  inputStr: string,
+  strList: string[]
+): boolean {
+  const normalizedStr = normalizeString(inputStr);
+
+  if (normalizeString) {
+    return strList.some((strEl) => {
+      return normalizeString(strEl) === normalizedStr;
+    });
+  }
+  return false;
+}
